74c569256860ed4c5bb435feed82015cf1a08f1d
Iwan-G <74765961+Iwan-G@users.noreply.github.com>
2023-09-28 00:01:00 +0800

    added nav bar to project

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Analytics from "./Analytics";
+import Earnings from "./Earnings";
+import FAQ from "./FAQ";
+import Navbar from "./Navbar";
+import Profile from "./Profile";
+import Transfers from "./Transfers";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <section className='section'>
+      <Navbar />
+      <div className="grid">
+        <div className="row__one">
+          <Analytics />
+          <FAQ />
+        </div>
+        <div className="row__two">
+          <Earnings />
+          <Transfers />
+          <Profile />
+        </div>
+      </div>
+    </section>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BiSearch } from "react-icons/bi";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <section className="nav">
+      <div className="title">
+        <h4>Hi Iwan,</h4>
+        <h1>
+          Welcome to <span>MY TAXI DASHBOARD</span>
+        </h1>
+      </div>
+      <div className="search">
+        <BiSearch />
+        <input type="text" placeholder="Search" />
+      </div>
+    </section>
+  );
 }
