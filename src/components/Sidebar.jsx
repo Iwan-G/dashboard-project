2368021d214e1a465b56f5279234915925da71ad
Iwan-G <74765961+Iwan-G@users.noreply.github.com>
2023-09-27 23:23:00 +0800

    Added side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,93 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./Sidebar.css";
+
+import { MdSpaceDashboard } from "react-icons/md";
+import { RiDashboard2Fill } from "react-icons/ri";
+import { FaAddressCard, FaTaxi } from "react-icons/fa";
+import { GiTwirlCenter } from "react-icons/gi";
+import { BsFillChatTextFill } from "react-icons/bs";
+import { IoSettings } from "react-icons/io5";
+import { FiLogOut } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { VscChromeClose } from "react-icons/vsc";
 
 export default function Sidebar() {
+  const [currentLink, setCurrentLink] = useState(1);
   return (
-    <div>Sidebar</div>
-  )
+    <>
+      <section className="section">
+        <div className="top">
+          <div className="brand">
+            <FaTaxi />
+            <span>MY TAXI</span>
+          </div>
+          <div className="toggle"></div>
+          <div className="links">
+            <ul>
+              <li
+                onClick={() => setCurrentLink(1)}
+                className={currentLink === 1 ? "active" : ""}
+              >
+                <a href="#">
+                  <MdSpaceDashboard />
+                  <span> Dashboard</span>
+                </a>
+              </li>
+              <li
+                onClick={() => setCurrentLink(2)}
+                className={currentLink === 2 ? "active" : ""}
+              >
+                <a href="#">
+                  <RiDashboard2Fill />
+                  <span> Riders</span>
+                </a>
+              </li>
+              <li
+                onClick={() => setCurrentLink(3)}
+                className={currentLink === 3 ? "active" : ""}
+              >
+                <a href="#">
+                  <FaAddressCard />
+                  <span> Payment Details</span>
+                </a>
+              </li>
+              <li
+                onClick={() => setCurrentLink(4)}
+                className={currentLink === 4 ? "active" : ""}
+              >
+                <a href="#">
+                  <GiTwirlCenter />
+                  <span> Learning Center</span>
+                </a>
+              </li>
+              <li
+                onClick={() => setCurrentLink(5)}
+                className={currentLink === 5 ? "active" : ""}
+              >
+                <a href="#">
+                  <BsFillChatTextFill />
+                  <span> FAQs</span>
+                </a>
+              </li>
+              <li
+                onClick={() => setCurrentLink(6)}
+                className={currentLink === 6 ? "active" : ""}
+              >
+                <a href="#">
+                  <IoSettings />
+                  <span> Settings</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="logout">
+          <a href="#">
+            <FiLogOut />
+            <span className="logout">Logout</span>
+          </a>
+        </div>
+      </section>
+    </>
+  );
 }
